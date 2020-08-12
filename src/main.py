d3d11120dca646e03e7a96c5bbf4297b38fe4b59
m-star18 <ryuuchei@ezweb.ne.jp>
2020-08-13 02:27:39 +0900

    Fix bug

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import PySimpleGUI as sg
+from PIL import Image
+import os
 
 from file import open_file
 from check import event_check
-from menu import main_menu, create_summary, Gamedata
+from menu import main_menu, create_summary
 from const import (
     FONT_SIZE,
     GAME_NAME_DATA_NUMBER,
@@ -46,6 +48,142 @@ def main(game_list_data=None, sum_number_data=0, event_data='', number_data=0, i
                                  window)
 
 
+class GameData:
+
+    def __init__(self, key, game_list):
+        self.name = key
+        self.genre = game_list[0]
+        self.date_birth = game_list[1]
+        self.company = game_list[2]
+        self.point = game_list[3]
+        self.image_site = game_list[4]
+        self.site = game_list[5]
+
+        if self.image_site != '':
+            self.image = Image.open(self.image_site)
+
+    def image_resize(self):
+        image_size = self.image.resize((500, 500))
+        image_size.save(self.image)
+
+    def run_data(self, window):
+        if self.site == 'site':
+            window['site'].update('エラー: 実行ファイルが指定されていません')
+
+        else:
+            os.system("xdg-open '" + self.site + "'")
+            exit()
+
+    def details_menu(self):
+        self.image_resize()
+
+        layout_details = [
+            [sg.Image(self.image),
+             ],
+            [sg.Text('ゲーム名', size=(10, 1), font=FONT_SIZE),
+             sg.Text(self.name, size=(17, 1), font=FONT_SIZE),
+             ],
+            [sg.Text('ジャンル', size=(10, 1), font=FONT_SIZE),
+             sg.Text(self.genre, size=(17, 1), font=FONT_SIZE),
+             ],
+            [sg.Text('発売年', size=(10, 1), font=FONT_SIZE),
+             sg.Text(f'{self.date_birth}年', size=(17, 1), font=FONT_SIZE),
+             ],
+            [sg.Text('会社名', size=(10, 1), font=FONT_SIZE),
+             sg.Text(self.company, size=(17, 1), font=FONT_SIZE),
+             ],
+            [sg.Text('最高得点', size=(10, 1), font=FONT_SIZE),
+             sg.Text(self.point, size=(17, 1), font=FONT_SIZE),
+             ],
+            [sg.CloseButton('戻る', size=(13, 1), font=FONT_SIZE, key='Exit'),
+             sg.Button(button_text='実行', size=(13, 1), font=FONT_SIZE, key='site'),
+             ],
+            [sg.Text('', size=(29, 1), font=FONT_SIZE),
+             ],
+        ]
+
+        return sg.Window(f'{self.name}の詳細').Layout(layout_details)
+
+    def add_menu(self):
+        genre_data = ['シューティング', 'アクション', 'アドベンチャー', 'ロールプレイング', 'パズル',
+                      'レース', 'シュミレーション', 'スポーツ', 'オープンワールド', 'ボード',
+                      ]
+        company_name_data = ['任天堂', 'コナミ', 'エニックス', 'ナムコ', 'ソニー']
+
+        # Edit and Add are determined by key
+        if self.point == '':
+            self.point = '0'
+            add_key = '追加'
+        else:
+            add_key = 'edit'
+
+        layout_add = [
+            [sg.Text('タイトル', size=(10, 2), font=FONT_SIZE),
+             sg.Input(default_text=self.name, size=(20, 2), font=FONT_SIZE),
+             ],
+            [sg.Text('ジャンル', size=(10, 2), font=FONT_SIZE),
+             sg.InputCombo(default_value=self.genre, values=genre_data, size=(20, 1),
+                           font=FONT_SIZE),
+             ],
+            [sg.Text('発売年', size=(10, 2), font=FONT_SIZE),
+             sg.Input(default_text=self.date_birth, size=(20, 2), font=FONT_SIZE),
+             ],
+            [sg.Text('会社名', size=(10, 2), font=FONT_SIZE),
+             sg.InputCombo(default_value=self.company, values=company_name_data, size=(20, 1),
+                           font=FONT_SIZE),
+             ],
+            [sg.Text('最高得点', size=(10, 2), font=FONT_SIZE),
+             sg.Input(default_text=self.point, size=(20, 2), font=FONT_SIZE),
+             ],
+            [sg.FileBrowse(button_text='画像を選択してください', size=(30, 1), font=FONT_SIZE, key=IMAGE_DATA_NUMBER,
+                           file_types=(('Image Files', '*.png'),)),
+             ],
+            [sg.FileBrowse(button_text='実行ファイルを選択してください', size=(30, 1), font=FONT_SIZE, key=SITE_DATA_NUMBER),
+             ],
+            [sg.Button(button_text='追加', size=(15, 1), font=FONT_SIZE, key=add_key),
+             sg.CloseButton('戻る', size=(15, 1), font=FONT_SIZE, key='Exit'),
+             ],
+            [sg.Text('', size=(30, 1), font=FONT_SIZE, key='INPUT')]
+        ]
+
+        return sg.Window('作成メニュー').Layout(layout_add)
+
+    def update_data(self, window):
+        while True:
+            event, new_game_data = window.Read()
+            key = list(new_game_data.values())
+
+            if event is None or event == 'Exit':
+                break
+
+            if new_game_data[SITE_DATA_NUMBER] == '':
+                new_game_data[SITE_DATA_NUMBER] = 'site'
+
+            if event == 'edit':
+                if new_game_data[IMAGE_DATA_NUMBER] == '':
+                    new_game_data[IMAGE_DATA_NUMBER] = self.image_site
+
+                if new_game_data[SITE_DATA_NUMBER] == '':
+                    new_game_data[SITE_DATA_NUMBER] = self.site
+
+            for i in range(NUMBER_DATA_PER):
+                if new_game_data[i] == '':
+                    window['INPUT'].update('入力忘れがあります')
+                    break
+
+            else:
+                return key
+
+
+    def update_details(self, window):
+        while True:
+            event, = window.Read()
+            if event is None:
+                break
+
+            self.run_data(window)
+
+
 class App:
 
     def __init__(self):
@@ -55,6 +193,7 @@ class App:
         self.number = 0
         self.get_load_data()
         self.sum_number = len(self.game_list)
+        self.flag = None
 
         self.window = MainMenu(self.number, self.sum_number, self.game_list, self.get_genre_data(),
                                self.get_date_birth_data(), self.get_company_data(),
@@ -62,62 +201,65 @@ class App:
 
     def get_load_data(self):
         for key in self.save_data.keys():
-            self.game_list.append(Gamedata(self.save_data.load(key)))
+            print(key, self.save_data.load(key))
+            self.game_list.append(GameData(key, self.save_data.load(key)))
 
     def get_genre_data(self):
         res = []
         for key in self.save_data.keys():
-            res.append(Gamedata(self.save_data.load(key)).genre)
+            res.append(GameData(key, self.save_data.load(key)).genre)
 
         return res
 
     def get_date_birth_data(self):
         res = []
         for key in self.save_data.keys():
-            res.append(Gamedata(self.save_data.load(key)).date_birth)
+            res.append(GameData(key, self.save_data.load(key)).date_birth)
 
         return res
 
     def get_company_data(self):
         res = []
         for key in self.save_data.keys():
-            res.append(Gamedata(self.save_data.load(key)).company)
+            res.append(GameData(key, self.save_data.load(key)).company)
 
         return res
 
     def add_game_data(self, key):
-        self.save_data.save(key, self.save_data.load(key))
-        self.__init__()
+        self.save_data.save(key[0], key[1:])
+        self.reload_game_data()
 
     def delete_game_data(self, key):
         self.save_data.delete(key)
-        self.__init__()
+        self.reload_game_data()
 
     def change_page_number(self, event):
         if event == 'next':
             self.number += 1
         else:
             self.number -= 1
+        self.reload_game_data()
+
+    def reload_game_data(self):
         self.window.close()
         self.__init__()
 
-    def get_event_check(self, event, window):
-        edit_data = []
-        flag = None
+    def get_event_check(self, event):
+        edit_data = [''] * NUMBER_DATA_PER
 
         if event == 'next' or event == 'previous':
             self.change_page_number(event)
 
         elif event == '再読込':
-            self.window.close()
-            self.__init__()
+            self.reload_game_data()
 
         for i in range(self.sum_number):
+            print(event, self.flag)
             if event == self.game_list[i].name:
-                flag = event
+                self.flag = event
                 self.window['INPUT'].update(f'{event}を選択中')
 
-            elif flag == self.game_list[i].name:
+            elif self.flag == self.game_list[i].name:
                 if event == '詳細':
                     window = self.game_list[i].details_menu()
                     self.game_list[i].update_details(window)
@@ -127,25 +269,21 @@ class App:
 
                 elif event == '編集':
                     window = self.game_list[i].add_menu()
-                    key, flag = self.game_list[i].update_data(window)
+                    key = self.game_list[i].update_data(window)
 
-                    if flag:
+                    if key:
                         self.add_game_data(key)
-                    else:
-                        self.delete_game_data(key)
 
         if event == '追加':
-            new = Gamedata(edit_data)
-            window = new.add_menu()
-            key, flag = new.update_data(window)
+            new_game_data = GameData(edit_data[0], edit_data[1:])
+            window = new_game_data.add_menu()
+            key = new_game_data.update_data(window)
 
-            if flag:
+            if key:
                 self.add_game_data(key)
-            else:
-                self.delete_game_data(key)
 
         elif event == '詳細' or event == '追加' or event == '編集' or event == '削除':
-            window['INPUT'].update('ゲームを選択してください')
+            self.window['INPUT'].update('ゲームを選択してください')
 
 
 class MainMenu:
@@ -231,4 +369,11 @@ class MainMenu:
 
 
 if __name__ == "__main__":
-    main()
+    a = App()
+    while True:
+        event, values = a.window.Read()
+        a.window['INPUT'].update('{0}を選択中'.format(event))
+        a.get_event_check(event)
+
+        if event is None:
+            exit()
