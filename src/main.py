53b9953444d79f31b6f5cb73878c8b9d758c0f13
m-star18 <31807@toyota.kosen-ac.jp>
2021-03-07 14:42:25 +0900

    Fix Update message

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ def main():
     app = App()
     while True:
         event, values = app.window.Read()
-        app.window['INPUT'].update('{0}を選択中'.format(event))
         app.get_event_check(event)
 
         if event is None:
